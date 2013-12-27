74c6934b649ad2aba26a7c5a35b379269228b84e
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2013-12-27 15:41:10 +0000

    tag c-icap-0.3.2 release

---
+++ c-icap-server/tags/c_icap_0_3_2/include/proc_mutex.h
@@ -0,0 +1,74 @@
+/*
+ *  Copyright (C) 2004-2008 Christos Tsantilas
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA.
+ */
+
+
+#ifndef __PROC_MUTEX_H
+#define __PROC_MUTEX_H
+
+#include "c-icap.h"
+#if defined (USE_SYSV_IPC_MUTEX)
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#elif defined (USE_POSIX_SEMAPHORES)
+#include <semaphore.h>
+#elif defined (USE_POSIX_FILE_LOCK)
+#include <fcntl.h>
+#elif defined (_WIN32)
+#include <windows.h>
+#endif
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#if defined (USE_SYSV_IPC_MUTEX)
+
+#define ci_proc_mutex_t int
+
+#elif defined (USE_POSIX_SEMAPHORES)
+
+#define ci_proc_mutex_t sem_t
+
+#elif defined (USE_POSIX_FILE_LOCK)
+
+#define FILE_LOCK_SIZE 25
+#define FILE_LOCK_TEMPLATE "/tmp/icap_lock.XXXXXX"
+
+typedef struct ci_proc_mutex{
+     char filename[FILE_LOCK_SIZE];
+     int fd;
+} ci_proc_mutex_t;
+
+#elif defined (_WIN32)
+
+#define ci_proc_mutex_t HANDLE
+
+#endif
+
+CI_DECLARE_FUNC(int) ci_proc_mutex_init(ci_proc_mutex_t *mutex);
+CI_DECLARE_FUNC(int) ci_proc_mutex_lock(ci_proc_mutex_t *mutex);
+CI_DECLARE_FUNC(int) ci_proc_mutex_unlock(ci_proc_mutex_t *mutex);
+CI_DECLARE_FUNC(int) ci_proc_mutex_destroy(ci_proc_mutex_t *mutex);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ c-icap-server/tags/c_icap_0_3_2/util.c
@@ -0,0 +1,69 @@
+/*
+ *  Copyright (C) 2004-2011 Christos Tsantilas
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA.
+ */
+
+#include "common.h"
+#include "c-icap.h"
+#include "util.h"
+#include <ctype.h>
+
+const char *ci_strnstr(const char *s, const char *find, size_t slen)
+{
+    size_t len = strlen(find);
+
+    if (len == 0)
+        return NULL;
+
+    while(len <= slen) {
+        if (*s == *find && strncmp(s, find, len) == 0 )
+            return s;
+        s++,slen--;
+    }
+    return NULL;
+}
+
+const char *ci_strcasestr(const char *str, const char *find)
+{
+    const char *s, *c, *f;
+    for (s = str; *s != '\0'; ++s) {
+        for (f = find, c = s; ; ++f, ++c) {
+            if (*f == '\0') /*find matched s*/
+                return s;
+            if (*c == '\0') /*find is longer than the remaining string */
+                return NULL;
+            if (tolower(*c) != tolower(*f))
+                break;
+        }
+    }
+    return NULL;
+}
+
+const char *ci_strncasestr(const char *s, const char *find, size_t slen)
+{
+    size_t len = strlen(find);
+
+    if (len == 0)
+        return NULL;
+
+    while(len <= slen) {
+        if (tolower(*s) == tolower(*find) && strncasecmp(s, find, len) == 0 )
+            return s;
+        s++,slen--;
+    }
+    return NULL;
+}
