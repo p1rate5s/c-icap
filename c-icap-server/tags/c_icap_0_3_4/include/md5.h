416dbc90ad85a254e6d800bb0aec779ab86ff59d
chtsanti <chtsanti@950cb5d8-81e4-40ab-8cb8-c719d5138fe1>
2014-07-21 10:24:38 +0000

    tag c-icap-0.3.4 release

---
+++ c-icap-server/tags/c_icap_0_3_4/include/md5.h
@@ -0,0 +1,16 @@
+#ifndef CI_MD5_H
+#define CI_MD5_H
+
+#include "c-icap.h"
+
+struct ci_MD5Context {
+        uint32_t buf[4];
+        uint32_t bits[2];
+        unsigned char in[64];
+};
+typedef struct ci_MD5Context ci_MD5_CTX;
+CI_DECLARE_FUNC(void) ci_MD5Init(struct ci_MD5Context *ctx);
+CI_DECLARE_FUNC(void) ci_MD5Update(struct ci_MD5Context *ctx, const unsigned char *buf, size_t len);
+CI_DECLARE_FUNC(void) ci_MD5Final(unsigned char digest[16], struct ci_MD5Context *ctx);
+
+#endif /* !CI_MD5_H */
